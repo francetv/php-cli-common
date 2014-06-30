29794a4c6fa200af0ccf88a19a0d0a4348d33c71
Olivier Hoareau <olivier@phppro.fr>
2014-07-01 00:07:02 +0200

    doc block

--- src/Ftven/Build/Common/Extension/Core/Feature/ExceptionThrowerTrait.php
+++ src/Ftven/Build/Common/Extension/Core/Feature/ExceptionThrowerTrait.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Cli-common package.
+ *
+ * (c) France Télévisions Editions Numériques <guillaume.postaire@francetv.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ftven\Build\Common\Extension\Core\Feature;
 
 /**
